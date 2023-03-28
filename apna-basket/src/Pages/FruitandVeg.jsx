a52443be93f4a750cde25672ccc53baa53751800
Deepak Kumar <deepakbhardwa0805@.com>
2023-03-28 23:00:28 +0530

    intro-commit

---
+++ apna-basket/src/Pages/FruitandVeg.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const FruitandVeg = () => {
+  return (
+    <div>FruitandVeg</div>
+  )
+}
---
+++ apna-basket/src/components/Carousel.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Carousel = () => {
+  return (
+    <div>Carousel</div>
+  )
+}
